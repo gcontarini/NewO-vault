a06f63ee58d6ece8886b438f865f71f7867197ba
Gabriel Contarini <gcontarini@gmail.com>
2022-05-07 16:14:46 +0100

    Change solidity compiler version for all contracts to the newest (8.13)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
   networks: {
     hardhat: {
       gas: 2100000000000000,
