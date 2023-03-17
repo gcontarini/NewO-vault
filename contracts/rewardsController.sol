4d70ba428518a79b2aa7a3fe44b96675987fa9d7
Gabriel Contarini <gcontarini@gmail.com>
2023-03-17 17:32:02 +0000

    Use formatter on controller and add more functionality

--- contracts/rewardsController.sol
+++ contracts/rewardsController.sol
@@ -5,6 +5,7 @@ import {Owned} from "./Owned.sol";
 import {IRewards} from "./interfaces/IRewards.sol";
 
 contract RewardsController is Owned {
+    // Sum to 32 bytes
     struct RewardsContract {
         address rewardsContractAddress;
         bool isAuth;
@@ -16,12 +17,12 @@ contract RewardsController is Owned {
     address[] public rewardsContracts;
     mapping(address => RewardsContract) public rewardsContractsAuth;
 
-	mapping (address => bool)[] public signedConditions;
-	uint public indexSigned = 0;
+    mapping(address => bool)[] public signedConditions;
+    uint public indexSigned = 0;
 
     /* ========== CONSTRUCTOR ========== */
 
-    constructor() {
+    constructor(address owner_) Owned(owner_) {
         signedConditions.push();
     }
 
@@ -82,37 +83,38 @@ contract RewardsController is Owned {
         emit RewardsContractRemoved(_rewardsContractAddress);
     }
 
-	/**
-    * @notice Sign the conditions
-    */
-   	function signConditions() public {
-		if (signedConditions[indexSigned][msg.sender]) revert ConditionsAlreadySigned();
-		signedConditions[indexSigned][msg.sender] = true;
-		emit ConditionsSigned(msg.sender);
-	}
+    /**
+     * @notice Sign the conditions
+     */
+    function signConditions() public {
+        if (signedConditions[indexSigned][msg.sender])
+            revert ConditionsAlreadySigned();
+        signedConditions[indexSigned][msg.sender] = true;
 
-	/**
-    * @notice Reset the sign conditions by creating a new mapping,
-	* points to the new mapping by incrementing indexSigned.
-    */
-   	function updateSignConditions() public onlyOwner{
-		signedConditions.push();
-		indexSigned++;
-		emit ConditionsUpdated();
-	}
+        emit ConditionsSigned(msg.sender);
+    }
 
+    /**
+     * @notice Reset the sign conditions by creating a new mapping,
+     * points to the new mapping by incrementing indexSigned.
+     */
+    function updateSignConditions() public onlyOwner {
+        signedConditions.push();
+        indexSigned++;
+
+        emit ConditionsUpdated();
+    }
 
     /* ========== IRewards ========== */
 
     /**
      * @notice Get all rewards from all rewards contracts
      * @dev This must never revert
-     * TODO add message signing
      */
-    function getAllRewards() public {
+    function getAllRewards() public onlySigned {
         for (uint256 i = 0; i < rewardsContracts.length; ) {
             IRewards rewardsContract = IRewards(rewardsContracts[i]);
-            rewardsContract.getRewards();
+            rewardsContract.getRewards(msg.sender);
 
             unchecked {
                 i++;
@@ -124,12 +126,11 @@ contract RewardsController is Owned {
      * @notice Notify all rewards contracts that a deposit has been made
      * @dev This function should be called after a deposit has been made
      * @dev This must never revert
-     * TODO add message signing
      */
-    function notifyAllDeposit() public {
+    function notifyAllDeposit() public onlySigned {
         for (uint256 i = 0; i < rewardsContracts.length; ) {
             IRewards rewardsContract = IRewards(rewardsContracts[i]);
-            rewardsContract.notifyDeposit();
+            rewardsContract.notifyDeposit(msg.sender);
 
             unchecked {
                 i++;
@@ -140,26 +141,27 @@ contract RewardsController is Owned {
     // LP contracts also have deposit. We need to think about it.
 
     /* ========== MODIFIERS ========== */
-	
+
     /**
-    * @notice Check if conditions are signed
-    */
-	modifier onlySigned() {
-		if (!signedConditions[indexSigned][msg.sender]) revert ConditionsNotSigned();
-		_;
-	}
+     * @notice Check if conditions are signed
+     */
+    modifier onlySigned() {
+        if (!signedConditions[indexSigned][msg.sender])
+            revert ConditionsNotSigned();
+        _;
+    }
 
     /* ========== EVENTS ========== */
 
     event RewardsContractAdded(address indexed rewardsContractAddress);
     event RewardsContractRemoved(address indexed rewardsContractAddress);
-	event ConditionsSigned(address indexed user);
-	event ConditionsUpdated();
+    event ConditionsSigned(address indexed user);
+    event ConditionsUpdated();
 
     /* ========== ERRORS ========== */
 
     error RewardsContractNotFound();
     error RewardsContractAlreadyExists();
-	error ConditionsAlreadySigned();
-	error ConditionsNotSigned();
+    error ConditionsAlreadySigned();
+    error ConditionsNotSigned();
 }
